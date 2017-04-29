2f7c0ffe6fd17ee6ec755b5a6ed9b32dcc139c69
emarkovic <enamarkovic27@gmail.com>
2017-04-29 14:29:34 -0700

    navbar is working

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import {render} from "react-dom";
 
+
+import Navbar from './navbar.jsx'
+
 export default class extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            activeTab: "home" 
+        }
+
+    }
+
+    handleTabClick(newTab) {
+        console.log(newTab)
+        this.setState({activeTab: newTab})
+        return
     }
 
     render() {
-        return <p>Hello world</p>
+        let content;
+        switch (this.state.activeTab) {
+            case "home":
+                content = <div className="page-content">Home page</div>
+                break;
+            case "search":
+                content = <div className="page-content">Search page</div>
+                break;
+            case "account":
+                content = <div className="page-content">Account page</div>
+                break;
+        }
+
+        return (
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs">
+                <Navbar activeTab={this.state.activeTab} handleTabClick={this.handleTabClick.bind(this)}/>                
+                <main className="mdl-layout__content">
+                    {content}
+                </main>
+            </div>
+        )
     }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,3 +4,4 @@ import {render} from "react-dom";
 import App from "./app.jsx";
 
 render(<App/>, document.getElementById("app"));
+

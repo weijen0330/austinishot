3eb6dfbfca91eecad03b24d89ff87c68154e497d
Wei-Jen <weijen0330@hotmail.com>
2017-05-01 16:34:42 -0700

    added Facebook webhook

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 // if "EADDRINUSE", do: $ sudo kill $(sudo lsof -t -i:80)
 global.__base = __dirname + '/';
 
-var db = require('./db')
-var app = require('./app')
+var db = require('./db');
+var app = require('./app');
 
 db.init().then(connection => {
-	app.start(connection)
+	app.start(connection);
 });

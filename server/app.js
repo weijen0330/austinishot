15e382174dc053ed9e44cbbdcd5c916d2b3ce859
Wei-Jen <weijen0330@hotmail.com>
2017-05-19 11:55:08 -0700

    added regex

--- server/app.js
+++ server/app.js
@@ -169,8 +169,18 @@ module.exports.start = function (connection) {
 
     /* Parses a string and returns an array of links if there are any. */
     function regParser(text) {
-        var re = new RegExp('/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/');
+        // Resource: https://gist.github.com/dperini/729294
+        var re = new RegExp('^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$');
+        var words = text.split().map(function(word) {
+            word.replace('<>', '');
+        });
         var links = [];
+        for (var word in words) {
+            if (re.test(word)) {
+               links.append(word);
+            }
+        }
+        return links;
     }
 
 
@@ -233,6 +243,7 @@ module.exports.start = function (connection) {
         request(oauthUrl, function (err, res, body) {
             console.log("getting access token");
             if (!err && res.statusCode === 200) {
+                var info = JSON.parse(body);
                 authConf.slack.accessToken = info.access_token;
             }
         });

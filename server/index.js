ad40a6c39cec1fa1c5cb6a31e2c978a1bd27eb62
emarkovic <enamarkovic27@gmail.com>
2017-05-25 20:34:58 -0700

    fixed database weirdness

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 // if "EADDRINUSE", do: $ sudo kill $(sudo lsof -t -i:80)
 global.__base = __dirname + '/';
 
-var db = require('./db')
+
 var app = require('./app')
 
-db.init().then(connection => {
-	app.start(connection)
-});
+var dbConfig = require(__base + 'secret/config-db.json');
+var MariaSql = require('mariasql');
+var bluebird = require('bluebird');
+var connection = bluebird.promisifyAll(new MariaSql(dbConfig));
+
+
+app.start(connection)

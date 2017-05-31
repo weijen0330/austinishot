7aeecba98931633f4a809e3225ebd92e44ffde77
emarkovic <enamarkovic27@gmail.com>
2017-05-30 22:25:32 -0700

    updating enpoints to use database data

--- server/database/message-db.js
+++ server/database/message-db.js
@@ -119,8 +119,7 @@ var MessageDB = {
 
 	},
 
-	getMessages(whereClause) {
-		console.log("options", options)
+	getMessages(whereClause, options) {		
 		return this._getObjects(
 			(
 				'SELECT ' +
@@ -135,34 +134,6 @@ var MessageDB = {
 					'l.type, ' +
 					'l.url, ' +
 					'l.img_url AS imgUrl, ' +
-					'd.domain_name AS domainName, ' +
-					't.tag_text AS tag ' + 
-				'FROM MESSAGE m ' + 
-				'JOIN PLATFORM p ON m.platform_id = p.platform_id ' + 
-				'JOIN LINKS l ON m.link_id = l.link_id ' + 
-				'JOIN DOMAIN d ON l.domain_id = d.domain_id ' + 
-				'JOIN LINKS_TAGS lt ON l.link_id = lt.link_id ' +
-				'JOIN TAGS t ON lt.tag_id = t.tag_id ' +
-				whereClause
-			), 
-			options
-		)
-	},
-
-	getUnreadMessages(userId) {
-		const query = (
-			'SELECT ' +
-					'm.message_id AS messageId, ' +
-					'm.sender, ' +
-					'm.note, ' +
-					'm.timeSent, ' +
-					'm.is_read AS isRead, ' +
-					'p.platform_name AS platformName, ' +
-					'l.title, ' +
-					'l.description, ' +
-					'l.type, ' +
-					'l.url, ' +
-					'l.img_url AS imageUrl, ' +
 					'd.domain_name AS domainName ' +
 					// 't.tag_text AS tag ' + 
 				'FROM MESSAGE m ' + 
@@ -171,12 +142,19 @@ var MessageDB = {
 				'JOIN DOMAIN d ON l.domain_id = d.domain_id ' + 
 				// 'JOIN LINKS_TAGS lt ON l.link_id = lt.link_id ' +
 				// 'JOIN TAGS t ON lt.tag_id = t.tag_id ' +
-				'WHERE m.recipient_id = :userId ' + 
-				'AND m.is_read = :isRead ' + 
-				'AND m.deleted = :deleted '
+				whereClause
+			), 
+			options
 		)
-		
-		return this._connection.queryAsync(query, {userId: 1, isRead: 0, deleted: 0})
+	},
+
+	getUnreadMessages(userId) {		
+		const whereClause = (
+			'WHERE m.recipient_id = :userId ' + 
+			'AND m.is_read = :isRead ' + 
+			'AND m.deleted = :deleted '
+		)
+		return this.getMessages(whereClause, {userId: 1, isRead: 0, deleted: 0})	
 	},	
 
 	getReadMessages(userId) {

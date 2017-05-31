9d86d2f0c9c6439ece0b50cb5a4a3624036adf19
emarkovic <enamarkovic27@gmail.com>
2017-05-31 02:35:20 -0700

    updating enpoints to use database data

--- server/routes/message-api.js
+++ server/routes/message-api.js
@@ -99,7 +99,7 @@ module.exports.Router = function (MessageDB) {
 
 	router.patch('/read/:messageId', (req, res, next) => {
 		const messageId = req.params.messageId
-		MessageDb.markRead(messageId).then(() => {
+		MessageDB.markRead(messageId).then(() => {
 			res.send("marking message with id: " + messageId + " as read")
 		})		
 	})

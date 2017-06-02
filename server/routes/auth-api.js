824db298e62e805f039d58baa864d01e00315d83
Wei-Jen <weijen0330@hotmail.com>
2017-06-02 05:54:06 -0700

    refactoring facebook code

--- server/routes/auth-api.js
+++ server/routes/auth-api.js
@@ -19,7 +19,7 @@ const authConf = {
         'clientID' : authTokens.fbClientID, // also refer to as app-ID
         'clientSecret' : authTokens.fbClientSecret, // also refer to as app-secret
         'scope' : 'email, public_profile, user_friends, user_posts',
-        'redirectUri' : 'https://lynxapp.me/api/auth/facebook'
+        'redirectUri' : 'https://lynxapp.me/api/auth/facebook_oauth'
     },
     'gmail' : {
         'clientID': authTokens.gmailClientID,
@@ -51,7 +51,7 @@ module.exports.Router = function (MessageDB, socketIo) {
     function regParser(message) {
         // Resource: https://gist.github.com/dperini/729294
         // defensive
-        console.log("message in reg parser", message)
+        console.log("message in reg parser", message);
         if (message) {            
             // Slack puts brackets around their links, so we need to remove them.
             var words = message.replace(/[<>]/g,'').split(' ');
@@ -106,59 +106,34 @@ module.exports.Router = function (MessageDB, socketIo) {
         }).then(() => linkSummary)        
     }
 
-
     // Facebook oauth: https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow
     router.get('/facebook_oauth', function(req, res) {
-        const oauthUrl = 'https://www.facebook.com/v2.9/dialog/oauth?client_id='
-            + authConf.facebook.clientID
-            + '&scope=' + authConf.facebook.scope
-            + '&redirect_uri=' + authConf.facebook.redirectUri;
-        res.redirect(oauthUrl);
-    });
+        if (!req.query.code) {
+            const oauthUrl = 'https://www.facebook.com/v2.9/dialog/oauth?client_id='
+                + authConf.facebook.clientID
+                + '&scope=' + authConf.facebook.scope
+                + '&redirect_uri=' + authConf.facebook.redirectUri;
 
-    router.get('/facebook', function(facebookReq, facebookRes) {
-        // Exchanging an authorization code for an API access token
-        const apiTokenUrl = 'https://graph.facebook.com/v2.9/oauth/access_token?client_id='
-            + authConf.facebook.clientID
-            + '&redirect_uri=' + authConf.facebook.redirectUri
-            + '&client_secret=' + authConf.facebook.clientSecret
-            + '&code=' + facebookReq.query.code;
-
-        request(apiTokenUrl, function (err, res, body) {
-            if (!err && res.statusCode == 200) {
-                const info = JSON.parse(body);
-                graph.setAccessToken(info.access_token);
-
-                const fboptions = {
-                    timeout: 3000,
-                    pool: { maxSockets:  Infinity },
-                    headers: { connection:  "keep-alive" }
-                };
-
-                const reqParam = {
-                    fields: 'type,caption,description,link,updated_time,from'
-                };
-
-                // Grab all the statuses on the feed.
-                graph.setOptions(fboptions).get("/me/feed", reqParam, function(err, res) {
-                    var messages = res.data;
-                    for (i = 0; i < messages.length; i++) {
-                        if (messages) {
-
-                        }
-                        var linkInfo = {
-                            platform : 'facebook'
-                        };
-
-
-                    }
-                    console.log(res);
-                });
-
+            if (!req.query.error) {
+                res.redirect(oauthUrl);
             } else {
-                console.error('Facebook API call error: ' + err);
+                res.status(401).send('Access denied to Facebook');
             }
-        });
+        } else {
+            // Exchanging an authorization code for an API access token
+            const apiTokenUrl = 'https://graph.facebook.com/v2.9/oauth/access_token?client_id='
+                + authConf.facebook.clientID
+                + '&redirect_uri=' + authConf.facebook.redirectUri
+                + '&client_secret=' + authConf.facebook.clientSecret
+                + '&code=' + req.query.code;
+
+            request(apiTokenUrl, function (err, apiTokenRes, body) {
+                if (!err && apiTokenRes.statusCode == 200) {
+                    const info = JSON.parse(body);
+                    graph.setAccessToken(info.access_token);
+                }
+            });
+        }
     });
 
     // Facebook webhook: https://developers.facebook.com/docs/graph-api/webhooks
@@ -177,76 +152,73 @@ module.exports.Router = function (MessageDB, socketIo) {
         //              value: 'This is an Example Status.' } ]
         const newStatus = facebookReq.body.entry[0].changes[0];
         
-        const statusValue = newStatus.value
+        const statusValue = newStatus.value;
         
-        const links = regParser(statusValue)       
+        const links = regParser(statusValue);
 
         if (links && links.length) {
-            var linkInfo = {
-                platform: "facebook",
-                sender: "",
-                bodyText: statusValue, 
-                timeStamp: Date.now()            
-            }
-            generateLinkSummary(links[0], linkInfo).then(linkSummary => {
-                return MessageDB.insertMessage(1, linkSummary)
-            }).then(message => {
-                socketIo.emit("new_message", {message: message});
-                // send the added message back to the user through web socket
-                // this should broadcast to users
-                facebookRes.status(200).send(message);
-            })
+            const fboptions = {
+                timeout: 3000,
+                pool: { maxSockets:  Infinity },
+                headers: { connection:  "keep-alive" }
+            };
+
+            // full scope: type,caption,description,link,updated_time,from,message
+            const reqParam = {
+                fields: 'type,caption,description,link,updated_time,from,message'
+            };
+
+            // Grab information about the status via the API
+            graph.setOptions(fboptions).get(newStatus.id, reqParam, function(err, res) {
+                if (err) {
+                    console.log("Error: not authorized with Facebook");
+                    res.redirect('/facebook_oauth');
+                } else {
+                    var message = res;
+                    console.log(res);
+
+                    if (res.type === 'video'|| res.type === 'link') {
+                        var linkInfo = {
+                            platform: 'facebook',
+                            sender: res.from.name,
+                            bodyText: facebookReq.body.entry[0].changes[0].value ? facebookReq.body.entry[0].changes[0].value : "",
+                            timeStamp: res.updated_time
+                        };
+                        console.log("link info", linkInfo);
+                        // generateLinkSummary(res.link, linkInfo).then(linkSummary => {
+                        //     // add the message to the database
+                        //     console.log("link summary:", linkSummary);
+                        //     return MessageDB.insertMessage(1, linkSummary)
+                        // }).then((message) => {
+                        //     console.log(message);
+                        //
+                        //     socketIo.emit("new_message", {message: message});
+                        //     // send the added message back to the user through web socket
+                        //     // this should broadcast to users
+                        //     facebookRes.status(200).send(message);
+                        // }).catch(console.log);
+                        facebookRes.status(200).send();
+                    } else {
+                        facebookRes.status(200).send("not a link");
+                    }
+                }
+            });
+
+            // var linkInfo = {
+            //     platform: "facebook",
+            //     sender: "",
+            //     bodyText: statusValue,
+            //     timeStamp: Date.now()
+            // };
+            // generateLinkSummary(links[0], linkInfo).then(linkSummary => {
+            //     return MessageDB.insertMessage(1, linkSummary)
+            // }).then(message => {
+            //     socketIo.emit("new_message", {message: message});
+            //     // send the added message back to the user through web socket
+            //     // this should broadcast to users
+            //     facebookRes.status(200).send(message);
+            // })
         }
-
-
-
-        // const fboptions = {
-        //     timeout: 3000,
-        //     pool: { maxSockets:  Infinity },
-        //     headers: { connection:  "keep-alive" }
-        // };
-        // // full scope: type,caption,description,link,updated_time,from,message
-        // const reqParam = {
-        //     fields: 'type,caption,description,link,updated_time,from,message'
-        // };
-
-        // // Grab information about the status via the API
-        // graph.setOptions(fboptions).get(newStatus.id, reqParam, function(err, res) {
-        //     console.log(err)
-        //     if (err) {
-        //         console.log("Error: Unable to get information from a single status from the API");
-        //         facebookRes.status(200).send();
-        //     } else {
-        //         var message = res;
-        //         console.log(res);
-
-        //         if (res.type === 'video'|| res.type === 'link') {
-        //             var linkInfo = {
-        //                 platform: 'facebook',
-        //                 sender: res.from.name,
-        //                 bodyText: facebookReq.body.entry[0].changes[0].value ? facebookReq.body.entry[0].changes[0].value : "",
-        //                 timeStamp: res.updated_time
-        //             };
-        //             console.log("link info", linkInfo)
-        //             // generateLinkSummary(res.link, linkInfo).then(linkSummary => {
-        //             //     // add the message to the database
-        //             //     console.log("link summary:", linkSummary);
-        //             //     return MessageDB.insertMessage(1, linkSummary)
-        //             // }).then((message) => {
-        //             //     console.log(message);
-        //             //
-        //             //     socketIo.emit("new_message", {message: message});
-        //             //     // send the added message back to the user through web socket
-        //             //     // this should broadcast to users
-        //             //     facebookRes.status(200).send(message);
-        //             // }).catch(console.log);
-        //             facebookRes.status(200).send();
-
-        //         } else {
-        //             facebookRes.status(200).send("not a link");
-        //         }
-        //     }
-        // });
     });
 
     // Gmail Oauth: https://developers.google.com/identity/protocols/OAuth2WebServer

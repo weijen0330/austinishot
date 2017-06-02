f0ea3f0b277d5f14472b963b7109c8f31b3bbcca
emarkovic <enamarkovic27@gmail.com>
2017-06-01 23:42:55 -0700

    fixing search

--- server/routes/message-api.js
+++ server/routes/message-api.js
@@ -40,67 +40,13 @@ module.exports.Router = function (MessageDB) {
 	router.get('/new', (req, res, next) => {
 		MessageDB.getUnreadMessages(1).then(messages => {
 			res.json(messages);
-		}).catch(next)
-
-		// const newMsgs = messages.filter(msg => {
-		// 	return !msg.isRead
-		// })
-		// res.json(newMsgs)		
+		}).catch(next)		
 	})
 
 	router.get('/old', (req, res, next) => {
 		MessageDB.getReadMessages(1).then(messages => {
 			res.json(messages)
 		}).catch(next)
-
-		// const oldMsgs = messages.filter(msg => {
-		// 	return msg.isRead
-		// })
-		// res.json(oldMsgs)
-	})
-
-	router.get('/tag/:tag', (req, res, next) => {
-		const tag = req.params.tag
-		// const userId = req.user.user_id
-		// MessageDB.getMessagesWithTag(userId, tag)
-
-		const msgs = messages.filter(msg => {
-			return msg.tags.includes(tag)
-		})
-		res.json(msgs)
-	})
-
-	router.get('/new/type/:type', (req, res, next) => {
-		const type = req.params.type
-		// const userId = req.user.user_id
-		// MessageDB.getNewMessagesWithType(userId, type)
-
-		const msgs = messages.filter(msg => {
-			return (msg.type == type) && !msg.isRead
-		})
-		res.json(msgs)
-	})
-
-	router.get('/old/type/:type', (req, res, next) => {
-		const type = req.params.type
-		// const userId = req.user.user_id
-		// MessageDB.getOldMessagesWithType(userId, type)
-		
-		const msgs = messages.filter(msg => {
-			return (msg.type == type) && msg.isRead
-		})
-		res.json(msgs)
-	})
-
-	router.get('/domain/:domain', (req, res, next) => {
-		const domain = req.params.domain
-		// const userId = req.user.user_id
-		// MessageDb.getMessagesWithDomain(userId, domain)
-		
-		const msgs = messages.filter(msg => {
-			return msg.domainName == domain
-		})
-		res.json(msgs)
 	})
 
 	router.patch('/read/:messageId', (req, res, next) => {
